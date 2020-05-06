3af55b5ce87789e64e988103706c8db35979cd80
Trend20 <enockomondi305@gmail.com>
2020-05-06 15:07:50 -0400

    card commit

--- src/Card.js
+++ src/Card.js
@@ -6,6 +6,11 @@ const Card = () =>{
         <div className="card">
             <img className="card-img-top"
             src={image} />
+            <div className="card-body">
+                <h5 className="card-title">Programming interface of the UI</h5>
+                <p className="card-text">This is the description of the whole bootstrap card</p>
+                <a href="#" className="btn btn-primary">Learn More</a>
+            </div>
         </div>
     )
 }
