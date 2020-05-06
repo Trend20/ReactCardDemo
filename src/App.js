4bcea21429878723f2f00b4e3fde78ae059e80f5
Trend20 <enockomondi305@gmail.com>
2020-05-06 15:25:09 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import Card from './Card';
 import image1 from './img/image1.jpg';
 
 
 function App(props) {
   return (
     <div className="App">
-      <Card 
+      <div className="row">
+        <div className="col-sm-4">
+        <Card 
         cardImage={image1}
         title="How to Become a Master in Javascript"
         description="This is the beginners guide to becoming super in Javascript Programming"
         link="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&cad=rja&uact=8&ved=2ahUKEwjqmerek5_pAhVEVhoKHWZXAcYQFjAFegQICxAG&url=https%3A%2F%2Fitnext.io%2Fwhat-is-props-
         and-how-to-use-it-in-react-da307f500da0&usg=AOvVaw2094RlAHeiQq7UW8X0bnwd"
       />
-      <Card 
+      </div>
+        <div className="col-sm-4">
+        <Card 
         cardImage={image1}
         title="How to Become a Master in Javascript"
         description="This is the beginners guide to becoming super in Javascript Programming"
-        link="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&cad=rja&uact=8&ved=2ahUKEwjqmerek5_pAhVEVhoKHWZXAcYQFjAFegQICxAG&url=https%3A%2F%2Fitnext.io%2Fwhat-is-
-        props-and-how-to-use-it-in-react-da307f500da0&usg=AOvVaw2094RlAHeiQq7UW8X0bnwd"
+        link="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&cad=rja&uact=8&ved=2ahUKEwjqmerek5_pAhVEVhoKHWZXAcYQFjAFegQICxAG&url=https%3A%2F%2Fitnext.io%2Fwhat-is-props-
+        and-how-to-use-it-in-react-da307f500da0&usg=AOvVaw2094RlAHeiQq7UW8X0bnwd"
       />
-      <Card 
+      </div>
+        <div className="col-sm-4">
+        <Card 
         cardImage={image1}
-        title="What are props?"
+        title="What are props in React?"
         description="This is the beginners guide to becoming super in Javascript Programming"
-        link="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&cad=rja&uact=8&ved=2ahUKEwjqmerek5_pAhVEVhoKHWZXAcYQFjAFegQICxAG&url=https%3A%2F%2Fitnext.io%2Fwhat-is-props
-        -and-how-to-use-it-in-react-da307f500da0&usg=AOvVaw2094RlAHeiQq7UW8X0bnwd"
+        link="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&cad=rja&uact=8&ved=2ahUKEwjqmerek5_pAhVEVhoKHWZXAcYQFjAFegQICxAG&url=https%3A%2F%2Fitnext.io%2Fwhat-is-props-
+        and-how-to-use-it-in-react-da307f500da0&usg=AOvVaw2094RlAHeiQq7UW8X0bnwd"
       />
+      </div>
+      </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import image from './img/blog-01.jpg';
 
 const Card = (props) =>{
     return(
         <div className="card">
             <img className="card-img-top"
-            src={props.image} />
+            src={props.cardImage} />
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}</p>
                 <a href={props.link} className="btn btn-primary">Learn More</a>
             </div>
         </div>
-    )
+    );
 }
 
 export default Card;
