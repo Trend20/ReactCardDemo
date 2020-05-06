6169bf2ced2bcff48fa1122eaed382966c001d17
Trend20 <enockomondi305@gmail.com>
2020-05-06 15:16:44 -0400

    output

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,11 @@ const Card = (props) =>{
     return(
         <div className="card">
             <img className="card-img-top"
-            src={image} />
+            src={props.image} />
             <div className="card-body">
-                <h5 className="card-title">Programming interface of the UI</h5>
-                <p className="card-text">This is the description of the whole bootstrap card</p>
-                <a href="#" className="btn btn-primary">Learn More</a>
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.description}</p>
+                <a href={props.link} className="btn btn-primary">Learn More</a>
             </div>
         </div>
     )
